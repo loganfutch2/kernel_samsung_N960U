d12b94e05a5499e9ba82c234baa3ac25eccca757
klabit87 <keith.labit@gmail.com>
2018-09-25 14:54:58 -0500

    Use crownqltechn_defconfig

--- build_kernel.sh
+++ build_kernel.sh
@@ -4,7 +4,7 @@ export CROSS_COMPILE=~/toolchains/aarch64-linux-android-4.9/bin/aarch64-linux-an
 export ANDROID_MAJOR_VERSION=o
 mkdir out
 
-make -C $(pwd) O=out -j8 ARCH=arm64 CROSS_COMPILE=~/toolchains/aarch64-linux-android-4.9/bin/aarch64-linux-android- KCFLAGS=-mno-android crownqlte_usa_single_defconfig
+make -C $(pwd) O=out -j8 ARCH=arm64 CROSS_COMPILE=~/toolchains/aarch64-linux-android-4.9/bin/aarch64-linux-android- KCFLAGS=-mno-android crownqlte_chn_open_defconfig
 make -C $(pwd) O=out -j8 ARCH=arm64 CROSS_COMPILE=~/toolchains/aarch64-linux-android-4.9/bin/aarch64-linux-android- KCFLAGS=-mno-android
 
 
