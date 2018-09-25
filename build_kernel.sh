51600c5e8ba07b5428ebe235249fa15894f19ac8
klabit87 <keith.labit@gmail.com>
2018-09-25 10:42:08 -0500

    Update build_kernel.sh toolchain location

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 export ARCH=arm64
-export CROSS_COMPILE=../PLATFORM/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export CROSS_COMPILE=~/toolchains/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 export ANDROID_MAJOR_VERSION=o
 mkdir out
 
-make -C $(pwd) O=out -j8 ARCH=arm64 CROSS_COMPILE=$(pwd)/../PLATFORM/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android- KCFLAGS=-mno-android crownqlte_usa_single_defconfig
-make -C $(pwd) O=out -j8 ARCH=arm64 CROSS_COMPILE=$(pwd)/../PLATFORM/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android- KCFLAGS=-mno-android
+make -C $(pwd) O=out -j8 ARCH=arm64 CROSS_COMPILE=~/toolchains/aarch64-linux-android-4.9/bin/aarch64-linux-android- KCFLAGS=-mno-android crownqlte_usa_single_defconfig
+make -C $(pwd) O=out -j8 ARCH=arm64 CROSS_COMPILE=~/toolchains/aarch64-linux-android-4.9/bin/aarch64-linux-android- KCFLAGS=-mno-android
 
 
 cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
