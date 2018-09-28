ffcd2dba382b5bf30ccbf42e9e707d68124ce6d7
klabit87 <keith.labit@gmail.com>
2018-09-27 19:09:08 -0500

    Fixup build script

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 export ARCH=arm64
-export CROSS_COMPILE=~/toolchains/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export CROSS_COMPILE=~/toolchains/note9/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 export ANDROID_MAJOR_VERSION=o
 mkdir out
 
-make -C $(pwd) O=out -j8 ARCH=arm64 CROSS_COMPILE=~/toolchains/aarch64-linux-android-4.9/bin/aarch64-linux-android- KCFLAGS=-mno-android crownqlte_chn_open_defconfig
-make -C $(pwd) O=out -j8 ARCH=arm64 CROSS_COMPILE=~/toolchains/aarch64-linux-android-4.9/bin/aarch64-linux-android- KCFLAGS=-mno-android
+make -C $(pwd) O=out -j8 ARCH=arm64 CROSS_COMPILE=~/toolchains/note9/aarch64-linux-android-4.9/bin/aarch64-linux-android- KCFLAGS=-mno-android crownqlte_chn_open_defconfig
+make -C $(pwd) O=out -j8 ARCH=arm64 CROSS_COMPILE=~/toolchains/note9/aarch64-linux-android-4.9/bin/aarch64-linux-android- KCFLAGS=-mno-android
 
 
 cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
